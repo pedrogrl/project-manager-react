4035d126eac4a7ebb39f4c0d073ddea804c62015
pedrogrl <pedro.gos@outlook.com>
2022-11-21 20:13:22 -0300

    inserção de dados na API

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,14 +1,36 @@
-import ProjectForm from '../project/ProjectForm'
-import styles from './NewProject.module.css'
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
 
-export default function NewProject(){
+import { useNavigate } from "react-router-dom";
+
+export default function NewProject() {
+
+  const navigate = useNavigate();
+
+  function newPost(project) {
+    // Initial values
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((data) => data.json())
+      .then((newData) => {
+        navigate("/projects");
+      })
+      .catch((error) => console.log(error));
+  }
 
   return (
     <div className={styles.newProjectContainer}>
       <h1>New Project</h1>
       <p>Create a new project then add services</p>
-      <ProjectForm btnText={'Create Project'}/>
+      <ProjectForm handleSubmit={newPost} btnText={"Create Project"} />
     </div>
-    
-  )
+  );
 }
